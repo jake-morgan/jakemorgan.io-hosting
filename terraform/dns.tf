9bc7cd3706e8de3299cc100c007d1c8f4dcedd35
Jake Morgan <jakeelliotmorgan@gmail.com>
2019-02-06 16:20:36 +0000

    added depends on to dns

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,20 +1,17 @@
-resource "azurerm_dns_zone" "main" {
-  name                = "${var.domain_name}"
-  resource_group_name = "dns"
-}
-
 resource "azurerm_dns_a_record" "at_record" {
   name                = "@"
-  zone_name           = "${azurerm_dns_zone.main.name}"
-  resource_group_name = "${azurerm_dns_zone.main.resource_group_name}"
+  zone_name           = "${var.domain_name}"
+  resource_group_name = "dns"
   ttl                 = 60
   records             = ["${azurerm_public_ip.main.ip_address}"]
+  depends_on          = ["azurerm_public_ip.main"]
 }
 
 resource "azurerm_dns_a_record" "www_record" {
   name                = "www"
-  zone_name           = "${azurerm_dns_zone.main.name}"
-  resource_group_name = "${azurerm_dns_zone.main.resource_group_name}"
+  zone_name           = "${var.domain_name}"
+  resource_group_name = "dns"
   ttl                 = 60
   records             = ["${azurerm_public_ip.main.ip_address}"]
+  depends_on          = ["azurerm_public_ip.main"]
 }
